40547687a8b28cbd8e83fa5f89b377af6e6371d9
Max Artemev <dan-pro352@ya.ru>
2018-09-27 16:47:47 +0400

    add example

---
+++ example/config_avito_example.py
@@ -0,0 +1,3 @@
+def config_dbsql():
+    config = "dbname='Имя базы данных' user='Имя пользователя' host='Адрес сервера' password='Пароль' port='Порт (если дефолтный, уберите данный параметр)'"
+    return config
